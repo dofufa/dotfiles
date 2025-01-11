1b7370d5b31afee0360c7d53c157e720150b1bc5
Dofufa Labs <121826629+dofufa@users.noreply.github.com>
2025-01-10 17:50:29 -0800

    Update 0-nix-setup.sh

--- 0-nix-setup.sh
+++ 0-nix-setup.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 ## basics
-sudo apt install -y tree jq yq nano mkdocs nodejs zip
+sudo apt install -y tree jq yq nano mkdocs nodejs zip npm
 sudo apt install -y ffmpeg imagemagick
 sudo apt install -y php8.2-gd php8.2-yaml php8.2-xml php8.2-json
 sudo apt install -y yt-dlp
-sudo apt install -y python3-poppler
 
 ## python-work
 sudo apt install -y python3-pil
@@ -12,7 +11,7 @@ sudo apt install -y python3-pil
 ## pdf work
 ## https://pypdf2.readthedocs.io/en/3.x/user/installation.html
 ## use python3 venv
-
+## sudo apt install -y python3-poppler
 
 
 ## data science
