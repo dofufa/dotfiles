775bcaf90d53255e33ac74940289926801941250
Dofufa Labs <121826629+dofufa@users.noreply.github.com>
2025-01-10 17:44:36 -0800

    Create 0-nix-setup.sh

---
+++ 0-nix-setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+## basics
+sudo apt install -y tree jq yq nano mkdocs nodejs zip
+sudo apt install -y ffmpeg imagemagick
+sudo apt install -y php8.2-gd php8.2-yaml php8.2-xml php8.2-json
+sudo apt install -y yt-dlp
+
+## python-work
+sudo apt install -y python3-pil
+
+## data science
+sudo apt install -y jupyter pandas
+
+## geodata
+#sudo apt install -y freecad
+
+
+
